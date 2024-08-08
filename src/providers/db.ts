1b316cfbb1b990afd448a1b4e420b6ea1dd363c7
M.Illiyin <55222179+milliyin@users.noreply.github.com>
2024-08-08 15:46:06 +0500

    Update db.ts

--- src/providers/db.ts
+++ src/providers/db.ts
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://bzpvyaqhcroqbstduylk.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.SUPABASE_KEY as string | undefined;
+
+if (!supabaseKey) {
+  throw new Error('SUPABASE_KEY is not set in environment variables');
+}
 export const supabase = createClient(supabaseUrl, supabaseKey)
