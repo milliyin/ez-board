c0a5f9c9b4d04094ccb55b75000285617797f633
M.Illiyin <55222179+milliyin@users.noreply.github.com>
2024-08-08 14:21:02 +0500

    Update page.tsx

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,3 +1,11 @@
+import React from 'react';
+
+type BoardPageProps = {
+    params: {
+        id: string;
+    };
+};
+
 export default function BoardPage({ params }: BoardPageProps){
     const { id } = params;  // 'id' is the dynamic part of your route
 
